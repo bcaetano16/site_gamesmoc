62f404599323f67c0fb2d50c422fdd288e44809c
BRUNO CAETANO SILVA <110364384+bcaetano16@users.noreply.github.com>
2022-09-08 12:26:04 -0300

    Adicionando responsividade para o codigo

--- cadastro.js
+++ cadastro.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 const form = document.getElementById('form-cadastro');
 
 
@@ -39,4 +40,47 @@ form.addEventListener('submit', function (e) {
         
         
 })
+=======
+const form = document.getElementById('form-cadastro');
+
+
+function validaNome(nomeCompleto) {
+    const nomeComoArray = nomeCompleto.split(' ');
+    return nomeComoArray.length >= 2;
+}
+
+function validaLogin() {
+    var Senha = document.getElementById('password').value;
+    var NovaSenha = document.getElementById('password2').value;
+    if (Senha == NovaSenha) {
+        validaLogin = true;
+    }
+    else validaLogin = false;
+
+}
+
+
+form.addEventListener('submit', function (e) {
+    let formEValido = false;
+    e.preventDefault();
+
+    const nomeBeneficiario = document.getElementById('nome-cliente');
+
+    formEValido = validaNome(nomeBeneficiario.value)
+    if (formEValido == false) {
+        
+    }
+    else
+        if (validaLogin == true) {
+        
+            nomeBeneficiario.value = '';
+        
+        alert("Cadastro realizado com sucesso");
+        
+        }
+            else if(validaLogin != true){(alert("As senhas são diferentes ou nome não está completo!"));}
+        
+        
+})
+>>>>>>> 47945aa75f4bc68a1b5d0c68c838d5da432f09f6
 console.log(form);
--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 const form = document.getElementById('form-login');
 
 
@@ -15,3 +16,22 @@ console.log(form);
 
 
 
+=======
+const form = document.getElementById('form-login');
+
+
+function validaLogin2() {
+    const Login = document.getElementById('login-usuario').value;
+    const Senha = document.getElementById('password-login').value;
+    if (Login == Senha) {
+        return true;
+    }
+    alert("Usuário logado com sucesso!");
+    return false;
+}
+
+console.log(form);
+
+
+
+>>>>>>> 47945aa75f4bc68a1b5d0c68c838d5da432f09f6

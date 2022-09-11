35ede5dc234d5fa4c91db975d99d1dff577968b9
BRUNO CAETANO SILVA <110364384+bcaetano16@users.noreply.github.com>
2022-09-11 10:10:44 -0300

    corrigindo bug página duplicada

--- main.js
+++ main.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 const form = document.getElementById('form-login');
 
 
@@ -16,22 +15,3 @@ console.log(form);
 
 
 
-=======
-const form = document.getElementById('form-login');
-
-
-function validaLogin2() {
-    const Login = document.getElementById('login-usuario').value;
-    const Senha = document.getElementById('password-login').value;
-    if (Login == Senha) {
-        return true;
-    }
-    alert("Usuário logado com sucesso!");
-    return false;
-}
-
-console.log(form);
-
-
-
->>>>>>> 47945aa75f4bc68a1b5d0c68c838d5da432f09f6
